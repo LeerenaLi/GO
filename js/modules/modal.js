eae775c810414018359f884f40f0cddf8d0e9d8e
Leerena <leerena7@gmail.com>
2022-10-25 09:37:40 +0300

    fixed modal

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,35 +1,52 @@
 const openModalBtn = document.querySelector('.header__btn');
 const modal = document.querySelector('.modal');
 const modalWrapper = document.querySelector('.modal__wrapper');
+const form = document.querySelector('#form-modal');
+const inputName = document.querySelector('#name');
+const inputPhone = document.querySelector('#phone');
 const closeElem = document.querySelector('.modal__close');
 const closeOver = document.querySelector('.modal__overlay');
-const closeSubmit = document.querySelector('.form-modal__btn');
 
-let heightWrapper = 0;
 
-if (heightWrapper < modalWrapper.scrollHeight) {
-    heightWrapper = modalWrapper.scrollHeight;
-}
+const modalControl = () => {
+    let heightWrapper = 0;
 
-openModalBtn.addEventListener('click', () => {
-    modal.classList.add('active');
-    modalWrapper.style.height =
-    modalWrapper.classList.contains('active') ?
-    '' : `${heightWrapper}px`;
-    modalWrapper.classList.add('active');
-});
+    if (heightWrapper < modalWrapper.scrollHeight) {
+        heightWrapper = modalWrapper.scrollHeight;
+    }
 
-closeElem.addEventListener('click', () => {
-    modal.classList.remove('active');
-    modalWrapper.classList.remove('active');
-});
+    const openModal = () => {
+        modal.classList.add('active');
+        modalWrapper.style.height =
+        modalWrapper.classList.contains('active') ?
+        '' : `${heightWrapper}px`;
+        modalWrapper.classList.add('active');
+    };
 
-closeOver.addEventListener('click', () => {
-    modal.classList.remove('active');
-    modalWrapper.classList.remove('active');
-});
+    openModalBtn.addEventListener('click', openModal);
 
-closeSubmit.addEventListener('click', () => {
-    modal.classList.remove('active');
-    modalWrapper.classList.remove('active');
-});
+    const closeModal = () => {
+        modal.classList.remove('active');
+        modalWrapper.classList.remove('active');
+    };
+
+    closeElem.addEventListener('click', closeModal);
+    closeOver.addEventListener('click', closeModal);
+
+    return {closeModal};
+};
+
+const {closeModal} = modalControl();
+
+const formControl = (closeModal) => {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        if (!inputName.value || !inputPhone.value) {
+            return;
+        } else {
+            closeModal();
+        }
+    });
+};
+formControl(closeModal);
+
